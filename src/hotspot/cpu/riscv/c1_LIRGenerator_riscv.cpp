8a615c8bd45102689d5a0a471aef18c12f1625a3
Fei Yang <yangfei@iscas.ac.cn>
2022-08-07 15:45:56 +0800

    Implement LIRGenerator::do_continuation_doYield for riscv

--- src/hotspot/cpu/riscv/c1_LIRGenerator_riscv.cpp
+++ src/hotspot/cpu/riscv/c1_LIRGenerator_riscv.cpp
@@ -302,7 +302,15 @@ void LIRGenerator::do_MonitorExit(MonitorExit* x) {
 }
 
 void LIRGenerator::do_continuation_doYield(Intrinsic* x) {
-  fatal("Continuation.doYield intrinsic is not implemented on this platform");
+  BasicTypeList signature(0);
+  CallingConvention* cc = frame_map()->java_calling_convention(&signature, true);
+
+  const LIR_Opr result_reg = result_register_for(x->type());
+  address entry = StubRoutines::cont_doYield();
+  LIR_Opr result = rlock_result(x);
+  CodeEmitInfo* info = state_for(x, x->state());
+  __ call_runtime(entry, LIR_OprFact::illegalOpr, result_reg, cc->args(), info);
+  __ move(result_reg, result);
 }
 
 // neg
