c3c63ee2d33193258c2a4378d30a1f84d10ebc68
yunyao.zxl <yunyao.zxl@alibaba-inc.com>
2022-09-06 17:27:22 +0800

    RISC-V: loom: fix *addr_at() -> at() in frame for frame::interpreter_frame_monitor_end

--- src/hotspot/cpu/riscv/frame_riscv.cpp
+++ src/hotspot/cpu/riscv/frame_riscv.cpp
@@ -311,7 +311,7 @@ BasicObjectLock* frame::interpreter_frame_monitor_begin() const {
 }
 
 BasicObjectLock* frame::interpreter_frame_monitor_end() const {
-  BasicObjectLock* result = (BasicObjectLock*) *addr_at(interpreter_frame_monitor_block_top_offset);
+  BasicObjectLock* result = (BasicObjectLock*) at(interpreter_frame_monitor_block_top_offset);
   // make sure the pointer points inside the frame
   assert(sp() <= (intptr_t*) result, "monitor end should be above the stack pointer");
   assert((intptr_t*) result < fp(),  "monitor end should be strictly below the frame pointer");
